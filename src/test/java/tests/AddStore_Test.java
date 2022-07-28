9f8a6e4ec5cc8a8678a8304cc2f9570d036ff8cf
maheshchinna <maheshchinnagavi@gmail.com>
2022-07-28 19:34:39 +0800

    minor fix

--- src/test/java/tests/AddStore_Test.java
+++ src/test/java/tests/AddStore_Test.java
@@ -1,24 +1,12 @@
 package tests;
 
 import io.appium.java_client.windows.WindowsDriver;
-
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import pages.Add_Account_Page;
-<<<<<<< HEAD
 import utilities.*;
-=======
-import utilities.ScreenshotCapture;
-import utilities.WaitForElement;
-import utilities.WinAppDriverSetUp;
-import utilities.GetSetJSONData;
-import java.io.IOException;
-import utilities.LoggingUtil;
-
->>>>>>> feature-change
-
 public class AddStore_Test extends GetSetJSONData{
 
     public static WindowsDriver driver;
@@ -34,11 +22,7 @@ public class AddStore_Test extends GetSetJSONData{
     }
 
     @Test(groups = "add_Store")
-<<<<<<< HEAD
     void addStore() throws Exception {
-=======
-    void addStore() throws InterruptedException, IOException {
->>>>>>> feature-change
         Add_Account_Page add_account_page = new Add_Account_Page(driver);
         add_account_page.enterStore(getValue("storeURL"));
         WebElement usernameElement = WaitForElement.getElementWithWaitId(driver, "username");
